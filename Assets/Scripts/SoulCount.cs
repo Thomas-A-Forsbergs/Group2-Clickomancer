615f577219893b0cd0250891f18a350815dd4f92
AWGcoding <anton.wahlstrom.gren@student.forsbergsskola.se>
2020-11-13 15:20:27 +0100

    Added text for cost/level and save function for cost/level

--- Assets/Scripts/SoulCount.cs
+++ Assets/Scripts/SoulCount.cs
@@ -1,4 +1,5 @@
-﻿using Unity.Mathematics;
+﻿﻿using System;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,18 +8,29 @@ public class SoulCount : MonoBehaviour
     
     [Header("Drag and Drop reference here")]
     public TMPro.TMP_Text soulText;
-    
+    public TMPro.TMP_Text upgradeText;
+    public TMPro.TMP_Text soulsPerClickText;
     [Header("Configurable values")]
     public int soulsPerClick = 1;
     public float upgradeMultiplier = 1.5f;
-    public int upgradeLevel = 0;
+    public int upgradeLevel = 0; 
     public int upgradeCost = 100;
     public float upgradeCostMultiplier = 1.1f;
     private int totalCost;
     
     private bool IsAffordable => Souls >= totalCost;
-    
-    
+
+    public void Start()
+    {
+        Display();
+        CalculateTotalCost();
+    }
+
+    public int UpgradeLevel
+    {
+        get => PlayerPrefs.GetInt("UpgradeLevel", 0);
+        set => PlayerPrefs.SetInt("UpgradeLevel", value);
+    }
     public int Souls
     {
         get => PlayerPrefs.GetInt("Souls", 0);
@@ -26,13 +38,13 @@ public class SoulCount : MonoBehaviour
     }
     public void Update()
     {
+        Display();
         CalculateTotalCost();
-        soulText.text = "Souls:" + Souls;
     }
 
     public void Click()
     {
-        Souls +=  Mathf.RoundToInt( 1 * (this.soulsPerClick * Mathf.Pow(upgradeMultiplier, upgradeLevel)));
+        Souls +=  Mathf.RoundToInt( 1 * (this.soulsPerClick * Mathf.Pow(upgradeMultiplier, UpgradeLevel)));
 
     }
     public void PurchaseUpgrade()
@@ -41,12 +53,19 @@ public class SoulCount : MonoBehaviour
         {
             return;
         }
-        upgradeLevel++;
+        UpgradeLevel++;
         Souls -= totalCost;
+        Display();
     }
-
     private void CalculateTotalCost()
     {
-        totalCost = Mathf.RoundToInt(upgradeCost * Mathf.Pow(upgradeCostMultiplier, upgradeLevel));
+        totalCost = Mathf.RoundToInt(upgradeCost * Mathf.Pow(upgradeCostMultiplier, UpgradeLevel));
+    }
+
+    private void Display()
+    {
+        upgradeText.text = $"Upgrade to level {UpgradeLevel +1 } Soul leech \nCost: {totalCost}";
+        soulsPerClickText.text = "Souls per click : " + Mathf.RoundToInt( 1 * (this.soulsPerClick * Mathf.Pow(upgradeMultiplier, UpgradeLevel)));
+        soulText.text = "Souls:" + Souls;
     }
 }
