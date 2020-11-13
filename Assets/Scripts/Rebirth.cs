a4aad53d1280ea037c926488d7ade1c5384ab879
Ali Shirzad <aliserzad@gmail.com>
2020-11-13 14:35:53 +0100

    Added Rebirth button, Texts, Code
    Edited the code in the Rebirth Script, attached it to a Empty Game object RebirthCount. Added to seprate text.

--- Assets/Scripts/Rebirth.cs
+++ Assets/Scripts/Rebirth.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.PlayerLoop;
 
 public class Rebirth : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Drag and Drop reference here")]
+    public TMPro.TMP_Text RebirthText;
+    public TMPro.TMP_Text BonusText;
+    public int rebirth;
+
+    
+    
+    public int Reborn
+    {
+        get => PlayerPrefs.GetInt("Rebirth", 0);
+        set => PlayerPrefs.SetInt("Rebirth", value);
+    }
+    
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RebirthText.text = "Rebirth:" + Reborn;
     }
 }
