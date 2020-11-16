8062d933f2b19fc3ddd846a5c55f8ebc525c0d3a
AWGcoding <anton.wahlstrom.gren@student.forsbergsskola.se>
2020-11-16 14:07:35 +0100

    Created text for mana and button for spell

---
+++ Assets/Scripts/ManaBar.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ManaBar : MonoBehaviour
+{
+    public TMPro.TMP_Text ManaText;
+    private int Mana = 100;
+
+}
