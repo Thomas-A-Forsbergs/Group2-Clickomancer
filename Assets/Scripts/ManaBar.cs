ebf4b45c83fc26bf77d6ab1bc3013061df7c536c
AWGcoding <anton.wahlstrom.gren@student.forsbergsskola.se>
2020-11-16 15:52:25 +0100

    Adding click function to consume mana

--- Assets/Scripts/ManaBar.cs
+++ Assets/Scripts/ManaBar.cs
@@ -1,11 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 using UnityEngine.UI;
 
 public class ManaBar : MonoBehaviour
 {
     public TMPro.TMP_Text ManaText;
-    private int Mana = 100;
+    public int currentMana = 100;
+    public int SpellCost = 80;
+    public int MaxMana = 100;
+    private bool IsAffordable => currentMana >= SpellCost;
 
+    public void UseSpell()
+    {
+        if (!IsAffordable)
+        {
+            return;
+        }
+
+        currentMana -= SpellCost;
+    }
+
+    public void Click()
+    {
+        UseSpell();
+    }
+
+    private void Update()
+    {
+        Display();
+    } 
+    private void Display()
+    {
+        ManaText.text = "Mana:" + currentMana + "/" + MaxMana;
+    }
 }
+
+
