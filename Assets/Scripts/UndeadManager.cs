3e03f7a1fdd565ab00f3f304b37d8cf6069b84e0
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-11-17 14:15:31 +0100

    Working for loop added to UndeadManager

--- Assets/Scripts/UndeadManager.cs
+++ Assets/Scripts/UndeadManager.cs
@@ -1,13 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class UndeadManager : MonoBehaviour
 {
+
+    public Undead[] undeadChildren;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        undeadChildren = GetComponentsInChildren<Undead>();
+        for (int i = 0; i < undeadChildren.Length; i++) 
+        {
+            undeadChildren[i].CalculateTotalProduction();
+        }
     }
 
     // Update is called once per frame
