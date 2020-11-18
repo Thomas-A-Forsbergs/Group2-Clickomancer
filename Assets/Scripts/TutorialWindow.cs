bfd32437e91d35b65fb4b82e8c138f18b9cc627d
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-11-18 10:18:45 +0100

    Programmed show/hide TutorialWindow Prefab and script
    now utilizes FirstTimePlaying to show/hide if the Prefab is located in the Scene

--- Assets/Scripts/TutorialWindow.cs
+++ Assets/Scripts/TutorialWindow.cs
@@ -1,18 +1,42 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TutorialWindow : MonoBehaviour
 {
+    private HelperClass helperClassRef;
+
+    public GameObject activateDeactivateTarget;
+    public Sprite dennisSprite;
+    
+    
+    
+    private void Awake()
+    {
+        helperClassRef = GameObject.Find("GameSystem").GetComponent<HelperClass>();
+        GetOfflineCurrency offlineCurrencyRef = helperClassRef.GetComponentInChildren<GetOfflineCurrency>();
+        if (offlineCurrencyRef.FirstTimePlaying != 1)
+        {
+            return;
+        }
+        DisplayWindow();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void DisplayWindow()
     {
-        
+        activateDeactivateTarget.SetActive(true);
+    }
+
+
+    public void CloseWindow()
+    {
+        activateDeactivateTarget.SetActive(false);
     }
 }
