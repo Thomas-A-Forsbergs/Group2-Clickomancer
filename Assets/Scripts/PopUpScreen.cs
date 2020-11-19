e5e0e5760836c1d41104f38e1a46c24e2198704c
AWGcoding <anton.wahlstrom.gren@student.forsbergsskola.se>
2020-11-19 11:36:31 +0100

    Added script for popup window

--- Assets/Scripts/PopUpScreen.cs
+++ Assets/Scripts/PopUpScreen.cs
@@ -3,7 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class PopUpScreen : MonoBehaviour
-{
-    
+public class PopUpScreen : MonoBehaviour {
+    public GameObject popUpBox;
+    private bool _showingPopUp;
+
+    public void ToggleScreen()
+    {
+        if (_showingPopUp)
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+
+        _showingPopUp = !_showingPopUp;
+    }
+    private void Show()
+    {
+        popUpBox.SetActive(true);
+    }
+
+    private void Hide()
+    {
+        popUpBox.SetActive(false);
+    }
 }
