e962fd9058c936eaa66c579f08af76abbb51c598
Linus Åkesson <linus.akesson@student.forsbergsskola.se>
2020-11-19 14:25:06 +0100

    Adds Offline Window Prefab
    Shows up in the page if not first time playing, and displays how much money you made since last you closed the game

---
+++ Assets/Scripts/OfflineBoxWindow.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OfflineBoxWindow : MonoBehaviour
+{
+    private HelperClass helperClassRef;
+
+    public GameObject activateDeactivateTarget;
+    public Sprite dennisSprite;
+    
+    
+    
+    private void Awake()
+    {
+        activateDeactivateTarget.SetActive(false);
+        
+        helperClassRef = GameObject.Find("GameSystem").GetComponent<HelperClass>();
+        GetOfflineCurrency offlineCurrencyRef = helperClassRef.GetComponentInChildren<GetOfflineCurrency>();
+        if (offlineCurrencyRef.FirstTimePlaying != 0)
+        {
+            return;
+        }
+        DisplayWindow();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void DisplayWindow()
+    {
+        activateDeactivateTarget.SetActive(true);
+    }
+
+
+    public void CloseWindow()
+    {
+        activateDeactivateTarget.SetActive(false);
+    }
+}
